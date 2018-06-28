57755876448bb9dc791aa2417e2bd1ef059fee1f
Abhay <Abhay>
2018-06-28 16:12:18 +0100

    service worker added: elniebiet curr conv is now a complete PWA online and offline

---
+++ sw.js
@@ -0,0 +1,50 @@
+//cache static assets
+const staticAssets = [
+	'./',
+	'./CurrencyConverter.js',
+	'./css/foundation.min.css',
+	'./css/main.css',
+	'./css/styles.css',
+];
+
+self.addEventListener('install', async event => {
+	const cache = await caches.open('CurrencyConverter');
+	cache.addAll(staticAssets);
+});
+self.addEventListener('fetch', event => {
+	const req = event.request;
+	const url = new URL(req.url);
+
+	if(url.origin == location.origin){
+		event.respondWith(cacheFirst(req));
+	}
+	else {
+		event.respondWith(networkFirst(req));
+	}
+});
+async function cacheFirst(req){
+	const cachedResponse = await caches.match(req);
+	return cachedResponse || fetch(req);
+}
+async function networkFirst(req){
+	//go to network, put in cache, if fails return that instead
+	const cache = await caches.open('CCDynamic');
+	try {
+		const res = await fetch(req);
+		cache.put(req, res.clone());
+		return res;
+	}
+	catch(error){
+		return await(cache.match(req));
+	}
+}
+//intercept fetch events
+// self.addEventListener('fetch', event => {
+// 	const req = event.request;
+// 	event.respondWith(cacheFirst(req));
+// });
+
+// async function cacheFirst(req){
+// 	const cachedResponse = await caches.match(req);
+// 	return cachedResponse || fetch(req);
+// }
