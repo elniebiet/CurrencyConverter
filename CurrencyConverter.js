7a5e4fe27c3c87c79f0da46fb08c8a9a6daa3b16
Abhay <Abhay>
2018-06-29 14:50:46 +0100

    Project fully completed, working both online and offline now

--- CurrencyConverter.js
+++ CurrencyConverter.js
@@ -1,3 +1,10 @@
+// this API is fully designed by ANIEBIET AKPAN @elniebiet 
+// this is a project for Andela 3.0 Google Scholarship.
+// completed on 29th June 2018
+// create indexedDB object and check platform compatibility
+
+let indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
+
 const src_web = "https://free.currencyconverterapi.com";
 let from_currency = "USD";
 let to_currency = "PHP";
@@ -5,12 +12,14 @@ let currencies = [];
 let symbols = [];
 let amt = 1;
 let rating = 0;
+let to_curr_index = 0;
 
 // /api/v5/convert?q=USD_PHP,PHP_USD&compact=ultra&apiKey=[YOUR_API_KEY] 
 //get rating "USD_PHP", "PHP_USD"
 
 const main = document.querySelector('#main');
 
+//function for onclick event of convert button
 async function convertNow(){
 	let curr_fr = document.getElementById('CURR_FR');
 	let curr_to = document.getElementById('CURR_TO');
@@ -22,18 +31,25 @@ async function convertNow(){
 
 	from_currency = curr_fr.options[curr_fr.selectedIndex].value;
 	to_currency = curr_to.options[curr_to.selectedIndex].value;
+
+	getCode(curr_to.options[curr_to.selectedIndex]); //get currency code from db
+	
 	await getRatingOnline(from_currency, to_currency);
 
 	amt = amnt_fr.value;
 	let res = rating*amt;
 	res = res.toFixed(2);
 
-	disp.innerHTML = `<input type="text" id="CURR_VAL" readonly placeholder="${res}" 
+
+	disp.innerHTML = `<input type="text" id="CURR_VAL" readonly placeholder="${res} ${to_currency}" 
 	style="background-color: #eee; font-weight: bold;" />`;
+
 }
 
 window.addEventListener('load', async e => {
 	getRatingsOnline();
+
+	//indexedDBSave();
 	if('serviceWorker' in navigator){
 		try {
 			navigator.serviceWorker.register('sw.js');
@@ -45,6 +61,7 @@ window.addEventListener('load', async e => {
 	}
 });
 
+//get the current currency ratings online
 async function getRatingOnline(from_currency = "USD", to_currency = "PHP"){
 	let res = await fetch(`${src_web}/api/v5/convert?q=${from_currency}_${to_currency}&compact=ultra&apiKey`);
 	let json = await res.json();
@@ -52,9 +69,11 @@ async function getRatingOnline(from_currency = "USD", to_currency = "PHP"){
 	rating = json[to_fr];
 }
 
+//called to get the ratings and currencies available online
 async function getRatingsOnline(){
 	getCurrencies(); 
 	getRatingOnline(from_currency, to_currency);
+
 }
 async function getCurrencies(){
 	let res = await fetch(`${src_web}/api/v5/currencies?apiKey`);
@@ -76,6 +95,71 @@ async function getCurrencies(){
 		}
 		else break;
 	}
+
+	indexedDBSave();
+}
+
+//create the indexed db and save the available currencies from the fetch
+function indexedDBSave(){
+
+	// Open (or create) the database
+	let open = indexedDB.open("CurrencyConverter", 1);
+
+	// Create the schema for the db
+	open.onupgradeneeded = function() {
+	    let db = open.result;
+	    let store = db.createObjectStore("storeCurrency", {keyPath: "id"});
+	    let index = store.createIndex("CurrencyIndex", ["currency.ind", "currency.code"]);
+	};
+
+	open.onsuccess = function() {
+		console.log('db opened successfully');
+	    // Start a new transaction
+	    let db = open.result;
+	    let tx = db.transaction("storeCurrency", "readwrite");
+	    let store = tx.objectStore("storeCurrency");
+	    let index = store.index("CurrencyIndex");
+	    let cnt = 0;
+
+	    // Add currency codes
+	    for(code of currencies){
+	    	store.put({id: cnt, currency: {ind: cnt, code: code}});
+	    	cnt++;
+	    }
+	    
+	}
+	open.onerror = function(){
+		console.log('db opening failed');
+	}
+	//close the db when transaction is done
+	tx.oncomplete = function() {
+        db.close();
+    };
+
+}
+
+//get the currency code from db by supplying its index
+function getCode(i){
+	let open = indexedDB.open("CurrencyConverter", 1);
+	open.onsuccess = function() {
+    // Start a new transaction
+    let db = open.result;
+    let tx = db.transaction("storeCurrency", "readwrite");
+    let store = tx.objectStore("storeCurrency");
+    let index = store.index("CurrencyIndex");
+
+    // Query the data
+    let getCurrency = store.get(i);
+
+    getCurrency.onsuccess = function() {
+        console.log(getCurrency.result.currency.code);  // => "John"
+    };
+
+    // Close the db when the transaction is done
+    tx.oncomplete = function() {
+        db.close();
+    };
+}
 }
 // async function getSymbols(){
 // 	let res = await fetch(`${src_web}/api/v5/currencies?apiKey`);
--- sw.js
+++ sw.js
@@ -1,4 +1,10 @@
-//cache static assets
+// this API is fully designed by ANIEBIET AKPAN @elniebiet 
+// this is a project for Andela 3.0 Google Scholarship.
+// completed on 29th June 2018
+// create indexedDB object and check platform compatibility
+
+
+//declare static assets
 const staticAssets = [
 	'./',
 	'./CurrencyConverter.js',
@@ -7,10 +13,14 @@ const staticAssets = [
 	'./css/styles.css',
 ];
 
+//cache all static assets
 self.addEventListener('install', async event => {
 	const cache = await caches.open('CurrencyConverter');
 	cache.addAll(staticAssets);
 });
+
+//attempt to fetch from internet if nw is available 
+//else visit the cache for cached values
 self.addEventListener('fetch', event => {
 	const req = event.request;
 	const url = new URL(req.url);
@@ -22,10 +32,15 @@ self.addEventListener('fetch', event => {
 		event.respondWith(networkFirst(req));
 	}
 });
+
+//returns whats available in cache if available 
+//else attempt to fetch from network
 async function cacheFirst(req){
 	const cachedResponse = await caches.match(req);
 	return cachedResponse || fetch(req);
 }
+
+//connect to network, if not available visit the cache
 async function networkFirst(req){
 	//go to network, put in cache, if fails return that instead
 	const cache = await caches.open('CCDynamic');
@@ -38,13 +53,3 @@ async function networkFirst(req){
 		return await(cache.match(req));
 	}
 }
-//intercept fetch events
-// self.addEventListener('fetch', event => {
-// 	const req = event.request;
-// 	event.respondWith(cacheFirst(req));
-// });
-
-// async function cacheFirst(req){
-// 	const cachedResponse = await caches.match(req);
-// 	return cachedResponse || fetch(req);
-// }
